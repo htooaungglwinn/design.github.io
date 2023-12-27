bddcf00e27c285378f5b6e9dfdb9a00998dbeb23
Kai <149455129+htooaungglwinn@users.noreply.github.com>
2023-12-27 17:01:00 +0630

    Fixing animation error on app

--- app.js
+++ app.js
@@ -283,48 +283,46 @@ class App {
 
       //   Project wrapper containers
       // const projectWrapperTags = document.querySelectorAll(".project__wrapper");
-      if (this.project__wrapper) {
-        const projectWrapperTags =
-          this.projectSection.querySelectorAll(".project__wrapper");
+      const projectWrapperTags =
+        this.projectSection.querySelectorAll(".project__wrapper");
 
-        // Looping the parent div and animating the childrens
-        projectWrapperTags.forEach((projectWrapper) => {
-          const dividerLine = projectWrapper.querySelector(".divider-line");
-          const footerLine = projectWrapper.querySelector(".footer-line");
-          const projectCta = projectWrapper.querySelector(".project__cta");
-          const projectHeader = projectWrapper.querySelector(
-            ".project__details h1"
-          );
-          const projectSubtitle = projectWrapper.querySelector("h3");
-          const projectDescription = projectWrapper.querySelector(
-            ".project-description"
-          );
+      // Looping the parent div and animating the childrens
+      projectWrapperTags.forEach((projectWrapper) => {
+        const dividerLine = projectWrapper.querySelector(".divider-line");
+        const footerLine = projectWrapper.querySelector(".footer-line");
+        const projectCta = projectWrapper.querySelector(".project__cta");
+        const projectHeader = projectWrapper.querySelector(
+          ".project__details h1"
+        );
+        const projectSubtitle = projectWrapper.querySelector("h3");
+        const projectDescription = projectWrapper.querySelector(
+          ".project-description"
+        );
 
-          const tl = gsap.timeline({
-            scrollTrigger: {
-              trigger: projectWrapper,
-              start: "-=250",
-              end: "",
-              duration: 0.3,
-            },
-          });
-
-          gsap.set(projectHeader, { y: 190 });
-          gsap.set(dividerLine, { scaleX: 0, transformOrigin: "left center" });
-          gsap.set(projectCta, { y: 70 });
-          gsap.set(footerLine, { scaleX: 0, transformOrigin: "left center" });
-          gsap.set(projectSubtitle, { opacity: 0 });
-          gsap.set(projectDescription, { opacity: 0 });
-
-          tl.to(projectHeader, { y: 0, ease: "back.out" })
-            .to(projectSubtitle, { opacity: 1 }, 0 + 0.1)
-            .to(projectDescription, { opacity: 1 }, 0 + 0.1)
-            .to(dividerLine, { scaleX: 1 }, 0 + 0.1)
-            .to(projectCta, { y: 0, ease: "back.out(2)" }, 0 + 0.1)
-            .to(footerLine, { scaleX: 1 }, 0 + 0.3);
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: projectWrapper,
+            start: "-=250",
+            end: "",
+            duration: 0.3,
+          },
         });
-        // end of condition
-      }
+
+        gsap.set(projectHeader, { y: 190 });
+        gsap.set(dividerLine, { scaleX: 0, transformOrigin: "left center" });
+        gsap.set(projectCta, { y: 70 });
+        gsap.set(footerLine, { scaleX: 0, transformOrigin: "left center" });
+        gsap.set(projectSubtitle, { opacity: 0 });
+        gsap.set(projectDescription, { opacity: 0 });
+
+        tl.to(projectHeader, { y: 0, ease: "back.out" })
+          .to(projectSubtitle, { opacity: 1 }, 0 + 0.1)
+          .to(projectDescription, { opacity: 1 }, 0 + 0.1)
+          .to(dividerLine, { scaleX: 1 }, 0 + 0.1)
+          .to(projectCta, { y: 0, ease: "back.out(2)" }, 0 + 0.1)
+          .to(footerLine, { scaleX: 1 }, 0 + 0.3);
+      });
+      // end of condition
     });
   }
 }
